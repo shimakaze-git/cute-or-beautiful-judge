575672d10e5d015a7b26f0d78989711a8c2728b2
shimakaze-git <shimakaze.soft+github@googlemail.com>
2018-11-14 12:26:25 +0900

    feat: kmeansを用いたクラスタリングなどを実践

--- api/make_traindatas/decomposition.py
+++ api/make_traindatas/decomposition.py
@@ -18,4 +18,43 @@ class Decomposition:
     # def print(test)
 
 if __name__ == '__main__':
-    pass
+    from PIL import Image
+    from skimage import data
+    import numpy as np
+    import os
+    from sklearn.decomposition import PCA
+    from sklearn.cluster import KMeans
+    import matplotlib.pyplot as plt
+
+    image = "../images/gray_faces/gray_4aa529273547b86d3521b358c1ad1e43.jpg"
+    image = "gray_4aa529273547b86d3521b358c1ad1e43.jpg"
+    as_arrayed_img = np.asarray(data.imread(f'../images/gray_faces/{image}'))
+    # print(as_arrayed_img.shape)
+
+    features = np.array([data.imread(f'../images/gray_faces/{path}') for path in os.listdir('../images/gray_faces')])
+    features = features.reshape(len(features), -1).astype(np.float64)
+    print(len(features))
+
+    pca = PCA(n_components=2)
+    pca.fit(features)
+    reduced = pca.fit_transform(features)
+    # print(reduced)
+    # print(reduced.shape)
+    # print(len(reduced))
+    # reduced = features
+
+    kmeans = KMeans(n_clusters=2).fit(reduced)
+    pred_label = kmeans.predict(reduced)
+    print(pred_label)
+    print(reduced[0])
+    print(reduced[1])
+    print(reduced[2])
+    print(reduced[4])
+    print(reduced[5])
+
+    # クラスタリングした結果をプロット
+    x = reduced[:, 0]
+    y = reduced[:, 1]
+    plt.scatter(x, y, c=pred_label)
+    plt.colorbar()
+    plt.show()
--- api/make_traindatas/face_extraciont.py
+++ api/make_traindatas/face_extraciont.py
@@ -13,15 +13,15 @@ import configparser
 class FaceExtraction:
     """ 顔画像抽出クラス """
 
-    inifile = configparser.ConfigParser()
-    inifile.read('../config.ini')
+    config = configparser.ConfigParser()
+    config.read('../config.ini')
 
     # 入力画像ディレクトリのパス。最後はスラッシュで終わる必要あり。
-    in_dir = inifile.get('extraction', 'in')
+    in_dir = config.get("extraction", 'in')
     # 出力先ディレクトリのパス。最後はスラッシュで終わる必要あり。
-    out_dir = inifile.get('extraction', 'out')
+    out_dir = config.get("extraction", 'out')
     # カスケードファイルのパス。
-    cascade_file = inifile.get('extraction', 'cascade')
+    cascade_file = config.get("extraction", 'cascade')
 
     def __init__(self):
         self.faceCascade = cv2.CascadeClassifier(self.cascade_file)
@@ -30,35 +30,28 @@ class FaceExtraction:
         """ 顔部分を抽出して画像にして保存 """
 
         # grayscale化
-        img = cv2.imread(self.in_dir + '/faces/' + file, cv2.IMREAD_COLOR)
+        img = cv2.imread(self.in_dir + file, cv2.IMREAD_COLOR)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
         # 顔部分抽出
         face = self.detect(gray)
 
         if len(face) is not 0:
-            for rect in face:
-                x = rect
-                cv2.rectangle(
-                    gray,
-                    tuple(rect[0:2]),
-                    tuple(rect[0:2]+rect[2:4]),
-                    (0, 0,255),
-                    thickness=2
-                )
+            (x, y, w, h) = face[0]
+            gray = gray[y:y+h, x:x+w]
 
             # 画像のリサイズ
             resized_image = self.resize(gray)
 
             # grayscale化して顔部分の保存
-            save_path = self.out_dir + '/gray_faces/' + 'gray_' + file +'.jpg'
-            cv2.imwrite(save_path, gray)
+            save_path = self.out_dir + 'gray_' + file
+            cv2.imwrite(save_path, resized_image)
 
     def detect(self, img):
         """ 顔部分の検出 """
-        face = self.faceCascade.detectMultiScale(img, 1.1, 3)
-        # face = self.faceCascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
-        
+        face = self.faceCascade.detectMultiScale(img)
+        # face = self.faceCascade.detectMultiScale(img, 1.1, 3)
+        # face = self.faceCascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=1, minSize=(1, 1))
         return face
 
     def resize(self, img):
@@ -72,9 +65,15 @@ class FaceExtraction:
 
 if __name__ == '__main__':
 
-    file = ""
-    obj = FaceExtraction()
-    obj.save_image(file)
+    import os
+
+    images_dir = os.listdir("../images/faces")
+
+    ### ../images/faces にある全ての画像ファイルの
+    ### 顔を抽出して../images/gray_facesに保存
+    for path in images_dir:
+        obj = FaceExtraction()
+        obj.save_image(path)
 
 # http://peaceandhilightandpython.hatenablog.com/entry/2016/02/18/194303
 # https://qiita.com/syoyo/items/5277906d29b2dd49e3a2#%E5%AD%A6%E7%BF%92%E3%81%97%E3%81%9F%E3%81%84
